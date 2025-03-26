8ddc9e6906c1027de7daa9245c1bdcd48381d1e4
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2025-03-25 18:59:38 -0700

    http server in TS

---
+++ app/main.ts
@@ -0,0 +1,19 @@
+import * as net from "net";
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+
+// Uncomment this to pass the first stage
+const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const request = data.toString();
+    const path = request.split(' ')[1];
+    const response = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1/ 400 Not Found\r\n\r\n';
+    socket.write(response)
+    socket.end();
+
+  })
+
+});
+
+server.listen(4221, "localhost");
